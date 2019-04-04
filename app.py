89cf9c104bee552c0abede010ede11b8716e0515
Pascal <molin.maths@gmail.com>
2019-04-04 17:39:12 +0200

    working version

--- app.py
+++ app.py
@@ -10,21 +10,13 @@ import time
 import random
 
 class User(dict):
-    """
-    name
-    date
-    """
+    """ name, date """
     def __init__(self, name):
         self['name'] = name
         self['date'] = time.time()
 
 class Answers(list):
-    """
-    user
-    quizz id
-    score
-    list choices
-    """
+    """ user, quizz id, score, list choices """
     def __init__(self, uid, qid, quizz):
         self.score = None
         self.uid = uid
@@ -37,21 +29,13 @@ class Answers(list):
     def set_score(self, score):
         self.score = score
 class Choice:
-    """
-    text
-    comment
-    """
+    """ text, comment """
     def __init__(self, **kwargs):
         self.text = kwargs.get('text','')
         self.comment = kwargs.get('comment','')
         self.correct = kwargs.get('correct',False)
 class Question(list):
-    """
-    title
-    text
-    list of choices
-    index correct
-    """
+    """ title, text, list of choices, index correct """
     def __init__(self, **kwargs):
         self.title = kwargs.get('title','')
         self.text =  kwargs.get('text','')
@@ -60,89 +44,68 @@ class Question(list):
             c = Choice(**c)
             self.append(c)
             if c.correct:
-                assert self.correct == None, """ current implementation assumes there is only one correct answer per question """
+                assert self.correct == None, """
+                    current implementation assumes there is only one
+                    correct answer per question """
                 self.correct = i
         assert self.correct != None, """ missing correct answer in question %s """ % self.text
     def grade(self, cid):
         """ cid a choice index, is choice number cid correct ? """
         return self.correct == cid
-
 class Quizz(list):
     """
     list of (questions, [index of choices])
     """
     pass
 
-class WebQuestion(Question):
-    def __init__(self, info, question, choices, selected = None):
-        (uid,aid,number) = info
+class PageData(dict):
+    template = 'invalid.html'
+    def __init__(self, info, **kwargs):
         self.info = info
-        self.title = question.title
-        self.number = number
-        self.text = question.text
-        for cid in choices:
-            #choice = WebChoice(question[cid])
-            self.append(question[cid])
-        self.selected = selected
+        dict.__init__(self, **kwargs)
+class PageConfirm(PageData):
+    template = 'confirm.html'
+class PageQuestion(PageData):
+    template = 'question.html'
+class PageScore(PageData):
+    template = 'score.html'
 
-class WebConfirm(list):
-    def __init__(self, info, user):
-        list.__init__(self, info)
-        self.info = info
-        self.user = user
-
-class WebScore(list):
-    def __init__(self, info, user, answers):
-        list.__init__(self, info)
-        self.info = info
-        self.user = user
-        self.answers = answers
-
-class QuizzApp:
-
-    usersdb = []
-    answersdb = []
-    questionsdb = []
-    quizzdb = []
+class QuizzApp(dict):
 
     def __init__(self):
-        pass
+        self['auth'] = {}
+        self['user'] = []
+        self['assignment'] = []
+        self['question'] = []
+        self['quizz'] = []
 
-    def get_answer(self, aid):
-        return self.answersdb[aid]
-    def get_user(self, uid):
-        return self.usersdb[uid]
-    def get_question(self, qid):
-        return self.questionsdb[qid]
-    def get_quizz(self, qid):
-        return self.quizzdb[qid]
+    def number_of(self,db):
+        assert db in self and isinstance(self[db], list)
+        return len(self[db])
 
-    def get_entry(self, db, index):
-        assert index < len(db), """ illegal index %i in table %s """ % (index, db.__name__) 
-        return db[index]
+    def get(self, db, index):
+        assert db in self and isinstance(self[db], list)
+        assert index < len(self[db]), """ illegal index %i in table %s """ % (index, db) 
+        return self[db][index]
 
-    def add_entry(self, db, entry):
-        db.append(entry)
-        return len(db) - 1
+    def add(self, db, entry):
+        assert db in self and isinstance(self[db], list)
+        self[db].append(entry)
+        return len(self[db]) - 1
 
-    def add_quizz(self, quizz):
-        self.quizzdb.append(quizz)
-        return len(self.quizzdb) - 1
-    def add_assignment(self, answers):
-        self.answersdb.append(answers)
-        return len(self.quizzdb) - 1
-
-    def new_user(self, name):
-        return self.add_entry(self.usersdb, User(name))
+    def new_user(self, name, auth):
+        """ return index uid """
+        assert auth not in self['auth'], "auth key %s already used"%auth
+        uid = self.add('user', User(name))
+        self['auth'][auth] = uid
+        return uid
 
     def new_quizz(self,length=3,maxanswers=3):
-        """
-        generates a new random quizz
-        """
+        """ generates a random quizz, return index qid """
         quizz = Quizz()
-        pool = len(self.questionsdb)
+        pool = self.number_of('question')
         for iq in random.sample(xrange(pool), length):
-            q = self.get_question(iq)
+            q = self.get('question',iq)
             # choose some choices, the first one is correct
             assert q.correct == 0, """ implementation assumes correct answer comes first, here correct = %i in question %s """ % (q.correct, q.text)
             c = random.sample(xrange(1,len(q)), maxanswers - 1)
@@ -150,33 +113,33 @@ class QuizzApp:
             random.shuffle(c)
             quizz.append( (iq, c) )
 
-        return self.add_quizz(quizz)
+        return self.add('quizz',quizz)
 
     def new_assignment(self, uid, qid):
-        quizz = self.get_quizz(qid)
+        """ return index aid of new assignment """
+        quizz = self.get('quizz',qid)
         answers = Answers(uid, qid, quizz)
-        return self.add_assignment(answers)
+        return self.add('assignment',answers)
 
     def post_answer(self, info, choice):
+        """ save answer and return index of next question """
         uid, aid, number = info
-        #user,answers,question = self.parse_token(token)
-        answers = self.get_answer(aid)
+        answers = self.get('assignment',aid)
         assert number < len(answers), """ wrong number %i >= len(assignment) """ % number
         if answers.confirmed:
             return None
         answers.set_choice(number, choice)
-        return (uid,aid,number+1)
-
-        #return self.get_question_data(self, (uid,aid,number+1))
+        return PageData((uid,aid,number+1))
 
     def post_confirmation(self, info):
+        """ mark assignment as done and return score """
         uid, aid = info
         #user,answers,question = self.parse_token(token)
-        answers = self.get_answer(aid)
+        answers = self.get('assignment',aid)
         answers.confirmed = True
-        return self.get_score_data(info)
+        return self.get_score_page(info)
 
-    def get_question_data(self, info):
+    def get_question_page(self, info):
         """
         info contains uid, aid, number
         returns the question data,
@@ -184,38 +147,53 @@ class QuizzApp:
         return confirmation page or score instead.
         """
         uid, aid, number = info
-        #user = self.get_user(uid)
-        answers = self.get_answer(aid)
-        quizz = self.get_quizz(answers.qid)
+        #user = self.get('user',uid)
+        answers = self.get('assignment',aid)
+        quizz = self.get('quizz',answers.qid)
 
         if number >= len(answers):
-            return self.get_score_data((uid,aid))
+            return self.get_score_page((uid,aid))
 
-        qid, cid = quizz[number]
-        question = self.get_question(qid)
-        return WebQuestion(info, question, cid, answers[number])
+        qid, choices_index = quizz[number]
+        question = self.get('question',qid)
+        choices = [ question[c] for c in choices_index ]
+        return PageQuestion(info,
+                   question=question,
+                   number = number,
+                   choices=choices,
+                   selected=answers[number],
+                   disabled=answers.confirmed)
 
     def grade(self, info):
         uid, aid = info
-        user = self.get_user(uid)
-        answers = self.get_answer(aid)
-        quizz = self.get_quizz(answers.qid)
+        user = self.get('user',uid)
+        answers = self.get('assignment',aid)
+        quizz = self.get('quizz',answers.qid)
         score = 0
         for choice, (qid, cid) in zip(answers,quizz):
-            print "choice %i in question %s"%(choice,(qid,cid))
-            question = self.get_question(qid)
+            if app.config['verbose']:
+                print "choice %i in question %s"%(choice,(qid,cid))
+            question = self.get('question',qid)
             score += question.grade(cid[choice])
         answers.set_score(score)
 
-    def get_score_data(self, info):
+    def get_score_page(self, info):
         uid, aid = info
-        user = self.get_user(uid)
-        answers = self.get_answer(aid)
+        user = self.get('user',uid)
+        answers = self.get('assignment',aid)
         if not answers.confirmed:
-            return WebConfirm(info, user)
+            return PageConfirm(info, user=user)
         if answers.score == None:
             self.grade(info)
-        return WebScore(info, user, answers)
+        results = {
+                'info': info,
+                'name': user['name'],
+                'score': answers.score,
+                'questions': len(answers)
+                }
+        return PageScore(info, user=user,
+                         answers=answers,
+                         results=results)
 
     def load_questions_yaml(self, filename):
         import yaml
@@ -223,8 +201,7 @@ class QuizzApp:
         with open(filename, 'r') as stream:
             try:
                 for q in yaml.load_all(stream):
-                    q = Question(**q)
-                    self.questionsdb.append(q)
+                    self.add('question',Question(**q))
             except yaml.YAMLError as exc:
                 print(exc)
 
@@ -234,124 +211,173 @@ class QuizzApp:
 #
 #####################################################################
 
-from flask import Flask, request, render_template, abort, redirect, url_for
+from flask import Flask, request, render_template, abort, redirect, url_for, send_file
 
-from itsdangerous import (URLSafeTimedSerializer
-                          as Serializer, BadSignature, SignatureExpired)
-
+from itsdangerous import (URLSafeTimedSerializer, Serializer, BadSignature, SignatureExpired, NoneAlgorithm)
 quizz = QuizzApp()
 app = Flask('quizz')
-app.config['SECRET_KEY'] = "lilalilalou"
-app.config['number_questions'] = 5
-app.config['number_choices'] = 3
 METHOD='GET'
 
+def get_serializer(salt=''):
+    if app.config['token_method'] == None:
+        return Serializer(app.config['SECRET_KEY'],salt=salt,signer=None)
+    elif app.config['token_method'] == 'default':
+        return URLSafeTimedSerializer(app.config['SECRET_KEY'],salt=salt)
+    else:
+        return URLSafeTimedSerializer(app.config['SECRET_KEY'],salt=salt)
+ 
 @app.template_filter('token')
 def tokenize(data, salt='quizz',expiration=600):
     """
     creates a token for the given data
     """
-    s = Serializer(app.config['SECRET_KEY'], salt = salt)
-            #, expires_in = expiration)
-    print "tokenize %s"%(data,)
+    s = get_serializer(salt)
+    if app.config['verbose']:
+        print "tokenize %s"%(data,)
     return s.dumps(data)
 
 def load_token(token, salt=''):
-    s = Serializer(app.config['SECRET_KEY'],salt=salt)
+    s = get_serializer(salt)
     try:
+        if app.config['verbose']:
+            print "parse token %s"%(token,)
         data = s.loads(token)
     except SignatureExpired:
+        # FIXME: return expiration page
+        if app.config['verbose']:
+            print "signature expired"
         return None # valid token, but expired
     except BadSignature:
+        if app.config['verbose']:
+            print "bad signature"
         return None # invalid token
+    if app.config['verbose']:
+        print "got %s"%(data,)
     return data
 
+from functools import wraps
+def token_required(f):
+    """
+    requires a valid token, whose salt
+    is the name of the embedded function
+    """
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token = request.args.get('token')
+        if app.config['verbose']:
+            print "[page %s, read access token]"%f.__name__
+        data = load_token(token, salt = f.__name__)
+        if data is None:
+            if app.config['verbose']:
+                print "[INVALID] got invalid token %s"%(token,)
+            return render_template("illegal.html"), 401
+        try:
+            return f(*args,data=data,**kwargs)
+        except AssertionError:
+            print "[INVALID] not in the database"
+            return render_template("illegal.html"), 401
+        except TypeError:
+            print "[TYPE ERROR] wrong entry"
+            return render_template("illegal.html"), 401
+    return decorated
 
+def render_page(data,route=None):
+    if data is None:
+        if app.config['verbose']:
+            print "[ILLEGAL] empty page"
+        return render_template('illegal.html')
+    if isinstance(data, PageData) and route is None:
+        return render_template(data.template, this=data)
+    elif route:
+        if app.config['verbose']:
+            print "[CREATE TOKEN] for page %s using data %s"%(route,data.info)
+        token = tokenize(data.info,salt=route)
+        return redirect(url_for(route, token=token))
+    else:
+        if app.config['verbose']:
+            print "[BUG] no route to render %s"%(data,)
+        return render_template('illegal.html')
+ 
 @app.route('/')
 def index():
-    return render_template('register.html')
-"""
-a user token contains
-- quizz: id
-- user: id
-plus a question number in order to answer that question
-"""
+    return render_template('token.html',auth=1)
 
-from functools import wraps
-def token_required(salt=''):
-    def decorator(f):
-        @wraps(f)
-        def decorated(*args,**kwargs):
-            if METHOD == 'POST':
-                token = request.form.get('token')
-            else:
-                token = request.args.get('token')
-            data = load_token(token, salt = salt)
-            if data is None:
-                return render_template("illegal.html"), 401
-            return f(*args,data=data,**kwargs)
-        return decorated
-    return decorator
+@app.route('/token', methods = [METHOD])
+def api_token(data=None):
+    return render_template('token.html',auth=1)
 
-def redirect_token(route,data,salt=''):
-    if data is None:
-        return render_template('illegal.html')
-    token = tokenize(data,salt=salt)
-    return redirect(url_for(route, token=token))
+@app.route('/activate', methods = [METHOD])
+@token_required
+def api_activate(data=None):
+    return render_template('register.html', auth=data)
 
-def render_page(page):
-    if isinstance(page, WebQuestion):
-        return render_template('question.html', this=page)
-    elif isinstance(page, WebConfirm):
-        return render_template('confirm.html', this=page)
-    elif isinstance(page, WebScore):
-        return render_template('score.html', this=page)
-    else:
-        return render_template('illegal.html', this=page)
-
-@app.route('/register', methods = [METHOD])
-@token_required(salt='register')
+@app.route('/api/register', methods = [METHOD])
+@token_required
 def api_register(data=None):
     """ token data (registration key) """
-    name = request.args.get('name')
-    uid = quizz.new_user(name)
+    auth = str(data)
+    name = unicode(request.args.get('name'))
+    uid = quizz.new_user(name, auth)
     qid = quizz.new_quizz(app.config['number_questions'],
                           app.config['number_choices'])
     aid = quizz.new_assignment(uid, qid)
-
-    print "created user %i,quizz %i,assignment %i"%(uid,qid,aid)
-    return redirect_token('api_question', (uid,aid,0), 'question')
+    if app.config['verbose']:
+        print "created user %i,quizz %i,assignment %i"%(uid,qid,aid)
+    return render_page(PageData((uid,aid,0)), 'api_question')
 
 @app.route('/question', methods = [METHOD])
-@token_required(salt='question')
+@token_required
 def api_question(data=None):
     """ token data (uid, aid, number) """
-    page = quizz.get_question_data(data)
+    page = quizz.get_question_page(data)
     return render_page(page)
 
 @app.route('/score', methods = [METHOD])
-@token_required(salt='score')
+@token_required
 def api_score(data=None):
     """ token data (uid, aid, number) """
-    page = quizz.get_score_data(data)
+    page = quizz.get_score_page(data)
     return render_page(page)
 
-@app.route('/answer', methods = [METHOD])
-@token_required(salt='answer')
+@app.route('/results', methods = [METHOD])
+@token_required
+def api_view(data=None):
+    """ token data (name, score, uid, aid) """
+    return render_template("results.html", this=data)
+
+@app.route('/api/answer', methods = [METHOD])
+@token_required
 def api_answer(data=None):
     """ token data (uid, aid, number) """
-    #choice = request.args.get('token')
     choice = int(request.args.get('choice'))
     data = quizz.post_answer(data, choice)
-    return redirect_token('api_question', data, 'question')
+    return render_page(data, 'api_question')
 
-@app.route('/submit', methods = [METHOD])
-@token_required(salt='submit')
+@app.route('/api/submit', methods = [METHOD])
+@token_required
 def api_submit(data=None):
     """ token data (uid, aid) """
     print 'received token %s' % data
     data = quizz.post_confirmation(data)
-    return redirect_token('api_score', data, 'score')
+    return render_page(data, 'api_score')
+
+from StringIO import StringIO
+import pyqrcode
+
+@app.route('/images/svg',methods = [METHOD])
+@token_required
+def api_svg(data=None):
+    stream = StringIO()
+    code = pyqrcode.create(data)
+    code.svg(stream, scale=1)
+    stream.seek(0)
+    return send_file(stream, mimetype='image/svg+xml')
+
+#####################################################################
+#
+# Command line application
+#
+#####################################################################
 
 import click
 
@@ -360,19 +386,29 @@ def cli():
     pass
 
 @cli.command()
-@click.option('--debug',default=True, help='turn on Flask debug mode')
-def server(debug):
+@click.option('--debug', default=False, help='turn on Flask debug mode')
+@click.option('--verbose', default=False, help='log requests')
+@click.option('--number_questions', default=5, help='number of questions per quizz')
+@click.option('--number_choices', default=3, help='number of choices per question')
+@click.option('--secret_key', default='lilalilalou', help='secret key')
+@click.option('--token_method', default='default', help='token method')
+def server(debug,verbose,number_questions,number_choices,secret_key,token_method):
+    app.config['verbose'] = verbose
+    app.config['SECRET_KEY'] = secret_key
+    app.config['number_questions'] = number_questions
+    app.config['number_choices'] = number_choices
+    app.config['token_method'] = token_method
     app.run(debug=debug)
 
 @cli.command()
 @click.option('--yaml',default='questions.yaml',
         help='load questions file')
 def build(yaml):
-   print quizz.get_question_data((uid,aid,0))
+    pass
 
 if __name__ == '__main__':
     quizz.load_questions_yaml('questions.yaml')
-    uid = quizz.new_user('Pascal')
+    uid = quizz.new_user('Pascal',1)
     qid = quizz.new_quizz(1)
     aid = quizz.new_assignment(uid, qid)
     cli()
